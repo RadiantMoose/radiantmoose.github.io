9feb25af689cd8966dbeb556fb4a5d8c07c5839d
Erik H ejh2958 <erik.halldorson@utexas.edu>
2025-05-01 23:53:14 -0500

    video link

--- js/video.js
+++ js/video.js
@@ -12,7 +12,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('video-container', {
         height: '315',
         width: '560',
-        videoId: 'QbtGv-joDJ8',
+        videoId: 'I6JjEX_pCMU',
     });
 }
 
